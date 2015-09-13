bde56373ca7fe8c94993dc13b6855c36151b8c85
zchee <zchee.io@gmail.com>
2015-09-13 22:54:54 +0900

    Fix source argv & Add log title
    Signed-off-by: zchee <zchee.io@gmail.com>

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -x
 
-source ./compilervars.sh
+source ./compilervars.sh intel64
 
+echo "========== LIBRARY_PATH =========="
 echo $LIBRARY_PATH
 
+echo "========== DYLD_LIBRARY_PATH =========="
 echo $DYLD_LIBRARY_PATH
 
+echo "========== PATH =========="
 echo $PATH
 
+echo "========== TARGET_ARCH =========="
 echo $TARGET_ARCH
 
+echo "========== TARGET_PLATFORM =========="
 echo $TARGET_PLATFORM
-
 
